0faf2811e8b913f2bed1ba2b0e534fc916e728e0
zundullahbd <uluzundullah@gmail.com>
2024-04-17 13:43:49 +0700

    fixing middleware

--- propen-a05/middleware.ts
+++ propen-a05/middleware.ts
@@ -1,5 +1,25 @@
-export { default } from 'next-auth/middleware';
+// export { default } from 'next-auth/middleware';
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: ['/dashboard', '/tickets', '/customers', '/products', '/brands', '/users', '/articles']
+// }
+
+
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth({
+  pages: {
+    signIn: '/auth/signin',
+  },
+})
+
+// The following is optional and will match the default behavior of Next.js
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard', '/tickets', '/customers', '/products', '/brands', '/users', '/articles']
+  matcher: (url: string) => {
+    if (url.startsWith('/api/auth/')) {
+      return false
+    }
+    return url.startsWith('/dashboard') || url.startsWith('/tickets') || url.startsWith('/customers') || url.startsWith('/products') || url.startsWith('/brands') || url.startsWith('/users') || url.startsWith('/articles')
+  },
 }
