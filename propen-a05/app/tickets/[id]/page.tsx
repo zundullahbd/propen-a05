046791e70e4aed49f612a84eeab4678625db1d54
zundullahbd <uluzundullah@gmail.com>
2024-04-17 07:56:39 +0700

    minor adjustment

--- propen-a05/app/tickets/[id]/page.tsx
+++ propen-a05/app/tickets/[id]/page.tsx
@@ -177,8 +177,8 @@ const TicketDetailPage = () => {
                                         <div className='mt-5'>
                                             <h2 className='text-[#667085] mb-2'>What they liked</h2>
                                             {dummyData.rating.likes.length > 0 && dummyData.rating.likes.map((like, index) => (
-                                                <div className='flex items-center space-x-3'>
-                                                    <svg key={index} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-[#3D3FDF]">
+                                                <div className='flex items-center space-x-3' key={index}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-[#3D3FDF]">
                                                         <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
                                                     </svg>
                                                     <p>{like}</p>
