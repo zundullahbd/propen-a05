80aef21992f22d540e49a57dfae5e52277bd414a
zundullahbd <uluzundullah@gmail.com>
2024-04-17 09:29:48 +0700

    minor adjustment

--- propen-a05/lib/auth.ts
+++ propen-a05/lib/auth.ts
@@ -7,7 +7,7 @@ import { compare } from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(db),
-    secret: process.env.SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     session: {
         strategy: "jwt",
         maxAge: 30 * 24 * 60 * 60
