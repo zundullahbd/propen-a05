8e87781ccf61335676b517f7c810cdcdad2cfb2b
zundullahbd <uluzundullah@gmail.com>
2024-04-17 14:00:54 +0700

    fixing middleware

--- propen-a05/middleware.ts
+++ propen-a05/middleware.ts
@@ -1,25 +1,67 @@
-// export { default } from 'next-auth/middleware';
-// // See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: ['/dashboard', '/tickets', '/customers', '/products', '/brands', '/users', '/articles']
-// }
-
-
-import { withAuth } from 'next-auth/middleware'
-
-export default withAuth({
-  pages: {
-    signIn: '/auth/sign-in',
-  },
-})
-
-// The following is optional and will match the default behavior of Next.js
+export { default } from 'next-auth/middleware';
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: (url: string) => {
-    if (url.startsWith('/api/auth/')) {
-      return false
-    }
-    return url.startsWith('/dashboard') || url.startsWith('/tickets') || url.startsWith('/customers') || url.startsWith('/products') || url.startsWith('/brands') || url.startsWith('/users') || url.startsWith('/articles')
-  },
+  matcher: ['/dashboard', '/tickets', '/customers', '/products', '/brands', '/users', '/articles']
 }
+
+
+// import { withAuth } from 'next-auth/middleware'
+
+// export default withAuth({
+//   pages: {
+//     signIn: '/auth/sign-in',
+//   },
+// })
+
+// // The following is optional and will match the default behavior of Next.js
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: (url: string) => {
+//     if (url.startsWith('/api/auth/')) {
+//       return false
+//     }
+//     return url.startsWith('/dashboard') || url.startsWith('/tickets') || url.startsWith('/customers') || url.startsWith('/products') || url.startsWith('/brands') || url.startsWith('/users') || url.startsWith('/articles')
+//   },
+// }
+
+// import { NextResponse } from "next/server";
+// import type { Session } from "next-auth";
+// import { getServerSession } from "next-auth"
+
+// export async function middleware(req: any) {
+//   // Get the session from the request
+//   const session: Session | null = await getServerSession(req);
+//   // If there is no session, return a 401 Unauthorized response
+//   if (!session) {
+//     return NextResponse.redirect("/sign-in");
+//   }
+
+//   // Get the requested route
+//   const { pathname } = req.nextUrl;
+
+//   // Define the routes that require specific roles
+//   const protectedRoutes = {
+//     "/tickets": ["admin", "exec"],
+//     "/dashboard": ["admin", "exec"],
+//     "/products": ["admin", "cs"],
+//     "/customers": ["admin", "cs", "sales"],
+//     "/users": ["admin"],
+//     "/brands": ["admin"],
+//     "/articles": ["admin"],
+//   };
+
+//   // If the requested route requires a specific role, check if the user has that role
+//   if (protectedRoutes[pathname as keyof typeof protectedRoutes]) {
+//     if (!protectedRoutes[pathname as keyof typeof protectedRoutes].includes(session.user.role)) {
+//       return new Response("Forbidden", { status: 403 });
+//     }
+//   }
+
+//   // If the user is authenticated and has the correct role, continue to the requested page
+//   return NextResponse.next();
+// }
+
+// // Apply the middleware to all routes except for the sign-in page
+// export const config = {
+//   matcher: "/((?!sign-in).*)",
+// };
